98690879c829872f0fccb68323d6aa57bf29cc4d
Sebadm20 <sebadm20@gmail.com>
2022-12-06 18:20:35 -0300

    Se crea la carpeta componentes donde trae el navbar y por otro lado la info que va a mostrar el mismo

---
+++ src/Components/MenuData.js
@@ -0,0 +1,37 @@
+export const MenuData = [ 
+
+{
+    tittle: "Home",
+    url: "#",
+    cName: "nav-links",
+    icon: "fa-solid fa-house-user"
+
+},
+{
+    tittle: "About",
+    url: "#",
+    cName: "nav-links",
+    icon: "fa-solid fa-circle-info"
+
+},
+{
+    tittle: "Shop",
+    url: "#",
+    cName: "nav-links",
+    icon: "fa-solid fa-cart-shopping"
+
+},
+{
+    tittle: "Contact",
+    url: "#",
+    cName: "nav-links",
+    icon: "fa-solid fa-adress-book"
+
+},
+{
+    tittle: "Sign-up",
+    url: "#",
+    cName: "nav-links-mobile"
+
+}
+]
