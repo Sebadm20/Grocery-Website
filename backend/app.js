7cb9d13e9ee54a1fc47df8673732e432d63cd99e
AlexVtr <alexiavetere@gmail.com>
2022-12-28 09:44:05 -0300

    1.1.2

--- backend/app.js
+++ backend/app.js
@@ -1,11 +1,18 @@
 const express = require('express')
+const { products__items } = require('./constants/products')
 const app = express()
 const port = 3000
+const id = req.params.id;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+app.get('/products', (req, res) => {
+
+  res.send(products__items)
+})
+app.get(`/product/${id}`, (req, res) => {
+  const filteredProduct = products__items.filter( product => product.id === id)
+  res.send(filteredProduct)
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`App listening on port ${port}`)
 })
--- backend/constants/products.js
+++ backend/constants/products.js
@@ -1,7 +1,32 @@
-export const products = [
+export const products__category =
     {
-        id: 1,
-        type: "Vegetables",
-        image: "",
+        type: String,
+        image_category: Any,
     }
-]
+
+export const product__stock =
+    {
+        stock_existence: Boolean,
+        stock_quantity: Number
+    }
+
+export const product_stationery =
+    {
+        is_stationery: Boolean,
+        stationery_months: [Number]
+    }
+
+export const products__items =
+{
+    id: Number,
+    category: products__category,
+    product: String,
+    brand: String,
+    image_item: Any,
+    image_food_example: Any,
+    price: Number,
+    stock: product__stock,
+    weight_in_gram: Number,
+    stationery: product_stationery,
+    
+}
